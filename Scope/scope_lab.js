ea1344ed55f8a690f513390a04198d4d7d1d9c9b
Jirayu Kaewprateep <jkaewprateep@gmail.com>
2024-03-08 13:35:13 -0500

    message

---
+++ Scope/scope_lab.js
@@ -0,0 +1,40 @@
+// Global scope
+var globalVar = "🧸💬 I'm a global variable";
+let globalLet = "🐑💬 ➰ I'm also global, but scoped with let";
+const globalConst = "🦭💬 I'm a global constant";
+
+
+{
+// Block scope
+var blockVar = "👧💬 🎈  I'm a block-scoped var";
+let blockLet = "👧💬 🎈  I'm a block-scoped let";
+const blockConst = "👧💬 🎈  I'm a block-scoped const";
+
+console.log(blockLet);
+}
+// Global scope
+console.log(globalVar); // Output: "I'm a global variable"
+console.log(globalLet); // Output: "I'm also global, but scoped with let"
+console.log(globalConst); // Output: "I'm a global constant"
+//Block Scope
+console.log(blockVar);
+// console.log(blockLet);
+
+function show(){
+    var functionVar = "🐨🎁🎵🎶  I'm a block-scoped var";
+    let functionLet = "🐯💬  I'm a block-scoped let";
+    const functionConst = "🐐💬  I'm a block-scoped const";
+
+    console.log(functionVar); // Throws ReferenceError
+    console.log(functionLet); // Throws ReferenceError
+    console.log(functionConst); // Throws ReferenceError
+    }
+    show();
+    
+    // console.log(functionVar); // Throws ReferenceError
+    // console.log(functionLet); // Throws ReferenceError
+    // console.log(functionConst); // Throws ReferenceError
+
+//
+console.log(blockVar);
+// console.log(blockLet);
