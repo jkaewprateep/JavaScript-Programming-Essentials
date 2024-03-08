7a79a2b66b51fc8a03a4d8fb62c014b359f1ea8a
Jirayu Kaewprateep <jkaewprateep@gmail.com>
2024-03-08 14:04:29 -0500

    update

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,46 @@
+let userRole = "admin";
+let accessLevel;
+
+if (userRole === "admin") {
+    accessLevel = " 🧸💬 Full access granted";
+} else if (userRole === "manager") {
+    accessLevel = "Limited access granted";
+} else {
+    accessLevel = "No access granted";
+}
+
+console.log("Access Level:", accessLevel);
+
+let isLoggedIn = true;
+let userMessage;
+
+if (isLoggedIn) {
+    if (userRole === "admin") {
+        userMessage = " 🧸💬 Welcome, Admin!";
+    } else {
+        userMessage = " 🧸💬 Welcome, User!";
+    }
+} else {
+    userMessage = " 🧸💬 Please log in to access the system.";
+}
+
+console.log("User Message:", userMessage);
+
+let userType = "subscriber";
+let userCategory;
+
+switch (userType) {
+    case "admin":
+        userCategory = " 🐐💬  Administrator";
+        break;
+    case "manager":
+        userCategory = " 🐐💬  Manager";
+        break;
+    case "subscriber":
+        userCategory = " 🐐💬  Subscriber";
+        break;
+    default:
+        userCategory = " 🐐💬  Unknown";
+}
+
+console.log("User Category:", userCategory);
