c2e41541d028508dc5465765d42a778dceb2ba66
Jirayu Kaewprateep <jkaewprateep@gmail.com>
2024-03-09 03:58:52 -0500

    measge

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -39,7 +39,8 @@ function endTest() {
     // Display the results
     var outputDiv = document.getElementById("output");
     outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
-        "<p> 🐑💬 ➰ Words Typed: " + typedWords + "</p>" +
+        "<p> 🐑💬 ➰ Total Length: " + userTypedText.length + "</p>" +
+        "<p>Words Typed: " + typedWords + "</p>" +
         "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
         "<p>Words Per Minute (WPM): " + wpm + "</p>";
 
