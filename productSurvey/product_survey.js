b20a5b797fe3fd9b53a80a78e967780e2159fba4
Jirayu Kaewprateep <jkaewprateep@gmail.com>
2024-03-09 02:00:51 -0500

    message

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -9,6 +9,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const experience = document.getElementById('userExperince').value;
 
     const submitButton=document.getElementById('submitBtn');
 
@@ -21,6 +22,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperience').innerHTML = experience;
 
     document.getElementById('userInfo').style.display = 'block';
 
